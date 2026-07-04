1ee207fa74e3099d4f0f4c94d98db3b30cb78393
automake <automake@mini.local>
2026-07-04 12:52:39 +0900

    ..

---
+++ embedding-bench/bench/setup-env.sh
@@ -0,0 +1,10 @@
+python3 -m venv .venv
+
+.venv/bin/pip install --upgrade pip
+.venv/bin/pip install sentence-transformers
+
+# To activate the virtual environment, run the following command:
+# source .venv/bin/activate
+
+# To deactivate the virtual environment, run the following command:
+# deactivate
